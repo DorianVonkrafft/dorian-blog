25427242f00e3a7acd92fb328c845ec0e8f25bf0
DorianVonkrafft <105909601+DorianVonkrafft@users.noreply.github.com>
2022-05-20 19:11:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import ReactDOM from "react-dom";
+import { BrowserRouter } from 'react-router-dom';
+import App from './Views/app';
+
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+//<StrictMode> : ne marche pas avec hookrouter
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    rootElement
+);
