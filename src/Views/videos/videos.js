09d036fad4414dd5b807003fa65f54ce3427eb04
DorianVonkrafft <105909601+DorianVonkrafft@users.noreply.github.com>
2022-05-22 00:34:21 +0200

    Update videos.js

--- src/Views/videos/videos.js
+++ src/Views/videos/videos.js
@@ -1,8 +1,15 @@
 import * as React from 'react';
 
 function Videos() {
-  return (
-    <p> Hello Word</p>
+  return ( 
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}>
+      <h1>WORK IN PROGRESS</h1>
+    </div>
   );
 }
 
